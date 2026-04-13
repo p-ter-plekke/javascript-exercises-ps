f5c761f5d769021d0ab1f90016b76901656b7d0e
p-ter-plekke <254936090+p-ter-plekke@users.noreply.github.com>
2026-04-13 10:37:57 +0200

    Add pseudo code

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+// I need a function that takes an array and some other (optional) arguments.
+// It should check for each argument whether it's present in the array.
+// If it's there, it should take that argument out of the array.
+// It should return the array without the arguments.
