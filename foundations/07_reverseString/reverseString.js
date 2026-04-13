60235ab79822e53b12171a588aa50bb058d42077
p-ter-plekke <254936090+p-ter-plekke@users.noreply.github.com>
2026-04-13 10:17:22 +0200

    Add pseudocode and function

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+    return str
+        .split('')
+        .reverse()
+        .join('');
 };
+
+console.log(reverseString("hello Prisca"));
+
+// I need a function that takes a string.
+// Breaks it down into letters.
+// Reverses the letter order.
+// Puts it back together into a new string.
+// Return the new string.
 
 // Do not edit below this line
 module.exports = reverseString;
