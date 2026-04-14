e144ce7b3844698b5e1fd99bf6a94b611252a12d
p-ter-plekke <254936090+p-ter-plekke@users.noreply.github.com>
2026-04-14 13:53:14 +0200

    Add pseudocode and solution for function

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+    if (!Number.isInteger(a)
+        || !Number.isInteger(b)
+        || a < 0
+        || b < 0) {
+        return "ERROR";
+    } else {
+        let smallToBig = [a, b].sort();
+        let first = smallToBig.at(0);
+        let last = smallToBig.at(1);
+        let sum = 0;
 
+        for (i = first; i <= last; i++) {
+            sum += i;
+        }
+        return sum;
+    }
 };
+
+sumAll(8, 3);
 
 // Do not edit below this line
 module.exports = sumAll;
+
+// I need a function that takes to integers as input.
+// Sets them in the order from small to big.
+// And then adds the first number, and then the second which is +1 and adds that, and so on.
+// Until the added number is bigger then the biggest input. 
+// Then it sould return the outcome.
